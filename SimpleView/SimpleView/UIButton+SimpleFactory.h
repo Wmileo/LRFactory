f1ecf980405316fb8f7439b27d8cb02cc0ba1cb4
chenruihua <chenruihua@mudi.com.cn>
2016-04-14 19:04:19 +0800

    uibutton

--- SimpleView/SimpleView/UIButton+SimpleFactory.h
+++ SimpleView/SimpleView/UIButton+SimpleFactory.h
@@ -10,25 +10,53 @@
 
 @interface UIButton (SimpleFactory)
 
+#pragma mark - 图片按钮
+
 /**
  *  工厂生产UIButton 图片背景
  */
-+(UIButton *)buttonWithFrame:(CGRect)frame image:(UIImage *)normal click:(void(^)())click onView:(UIView *)view;
++(UIButton *)buttonWithFrame:(CGRect)frame normalImage:(UIImage *)normalImg click:(void(^)())click onView:(UIView *)view;
 
 /**
- *  工厂生产UIButton 图片背景 设置中点
+ *  工厂生产UIButton 图片背景 设置中点 大小根据图片大小显示
  */
-+(UIButton *)buttonWithCenter:(CGPoint)center image:(UIImage *)image click:(void(^)())click onView:(UIView *)view;
++(UIButton *)buttonWithCenter:(CGPoint)center normalImage:(UIImage *)normalImg click:(void(^)())click onView:(UIView *)view;
+
+/**
+ *  添加点击高亮图片
+ */
+-(UIButton *)addHighlightedImage:(UIImage *)highlightedImg;
+
+#pragma mark - 文字按钮
 
 /**
  *  工厂生产UIButton 颜色背景 文字标题
  */
-+(UIButton *)buttonWithFrame:(CGRect)frame bgColor:(UIColor *)bgColor title:(NSString *)title textColor:(UIColor *)textColor click:(void(^)())click onView:(UIView *)view;
++(UIButton *)buttonWithFrame:(CGRect)frame title:(NSString *)title textColor:(UIColor *)textColor font:(UIFont *)font click:(void(^)())click onView:(UIView *)view;
+
+/**
+ *  添加点击高亮图片
+ */
+-(UIButton *)addHighlightedTitle:(NSString *)title textColor:(UIColor *)textColor;
+
+/**
+ *  工厂生产UIButton 颜色背景 文字标题  设置中点 大小根据图片大小显示
+ */
++(UIButton *)buttonWithCenter:(CGPoint)center title:(NSString *)title textColor:(UIColor *)textColor font:(UIFont *)font click:(void(^)())click onView:(UIView *)view;
+
+#pragma mark - 空按钮
 
 /**
  *  工厂生产UIButton 空按钮
  */
 +(UIButton *)buttonEmptyWithFrame:(CGRect)frame click:(void(^)())click onView:(UIView *)view;
 
+
+#pragma mark - 
+
+/**
+ *  设置背景颜色
+ */
+-(UIButton *)resetBackgroundColor:(UIColor *)color;
 
 @end
--- SimpleView/SimpleView/UIButton+SimpleFactory.m
+++ SimpleView/SimpleView/UIButton+SimpleFactory.m
@@ -7,9 +7,74 @@
 //
 
 #import "UIButton+SimpleFactory.h"
+#import "NSObject+Block.h"
 
 @implementation UIButton (SimpleFactory)
 
+#pragma mark - 图片按钮
 
++(UIButton *)buttonWithFrame:(CGRect)frame normalImage:(UIImage *)normalImg click:(void (^)())click onView:(UIView *)view{
+    UIButton *button = [[UIButton alloc] initWithFrame:frame];
+    if (normalImg) {
+        [button setImage:normalImg forState:UIControlStateNormal];
+    }
+    [button setExclusiveTouch:YES];
+    if (click) [button onlyHangdleUIControlEvent:UIControlEventTouchUpInside withBlock:click];
+    if (view) [view addSubview:button];
+    return button;
+}
+
++(UIButton *)buttonWithCenter:(CGPoint)center normalImage:(UIImage *)normalImg click:(void (^)())click onView:(UIView *)view{
+    
+    return [UIButton buttonWithFrame:CGRectMake(center.x - normalImg.size.width / 2, center.y - normalImg.size.height / 2, normalImg.size.width, normalImg.size.height) normalImage:normalImg click:click onView:view];
+
+}
+
+-(UIButton *)addHighlightedImage:(UIImage *)highlightedImg{
+    [self setImage:highlightedImg forState:UIControlStateHighlighted];
+    return self;
+}
+
+#pragma mark - 文字按钮
+
++(UIButton *)buttonWithFrame:(CGRect)frame title:(NSString *)title textColor:(UIColor *)textColor font:(UIFont *)font click:(void (^)())click onView:(UIView *)view{
+    UIButton *button = [[UIButton alloc] initWithFrame:frame];
+    if(title){
+        [button setTitle:title forState:UIControlStateNormal];
+    }
+    [button setExclusiveTouch:YES];
+    if (font) button.titleLabel.font = font;
+    if (textColor) [button setTitleColor:textColor forState:UIControlStateNormal];
+    if (click) [button onlyHangdleUIControlEvent:UIControlEventTouchUpInside withBlock:click];
+    if (view) [view addSubview:button];
+    return button;
+}
+
+-(UIButton *)addHighlightedTitle:(NSString *)title textColor:(UIColor *)textColor{
+    [self setTitle:title forState:UIControlStateHighlighted];
+    if (textColor) [self setTitleColor:textColor forState:UIControlStateHighlighted];
+    return self;
+}
+
+CGSize CGSizeMakeTitleSize(NSString *title, UIFont *font, CGFloat maxWidth){
+    return [title boundingRectWithSize:CGSizeMake(maxWidth, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;
+}
+
++(UIButton *)buttonWithCenter:(CGPoint)center title:(NSString *)title textColor:(UIColor *)textColor font:(UIFont *)font click:(void (^)())click onView:(UIView *)view{
+    CGSize size = CGSizeMakeTitleSize(title, font, 300);
+    return [UIButton buttonWithFrame:CGRectMake(center.x - size.width / 2, center.y - size.height / 2, size.width, size.height) title:title textColor:textColor font:font click:click onView:view];
+}
+
+#pragma mark - 空按钮
+
++(UIButton *)buttonEmptyWithFrame:(CGRect)frame click:(void (^)())click onView:(UIView *)view{
+    return [UIButton buttonWithFrame:frame title:nil textColor:nil font:nil click:click onView:view];
+}
+
+#pragma mark - 
+-(UIButton *)resetBackgroundColor:(UIColor *)color{
+    self.backgroundColor = color;
+    return self;
+}
 
 @end
