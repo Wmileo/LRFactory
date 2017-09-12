e21a3cdfabced2ea2f382a68f029a7a0cb96cd8a
ileo <work.mileo@gmail.com>
2017-09-12 11:42:49 +0800

    tagobject

--- SimpleView/ViewController.m
+++ SimpleView/ViewController.m
@@ -29,7 +29,7 @@
     
     self.navBackgroundColor = [UIColor whiteColor];
     self.view.backgroundColor = [UIColor whiteColor];
-    self.navigationBarHidden = NO;
+    self.navBarHidden = NO;
 //    self.statusHide = YES;
     
     self.navShadowImage = [[UIImage alloc] init];
