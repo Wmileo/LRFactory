9ebd2c1ab1619269cdf26cb0ec904418804fec77
leo <work.mileo@gmail.com>
2018-05-14 10:34:39 +0800

    1.0.7

--- SimpleView/SimpleView/UIViewController+SimplePresent.m
+++ SimpleView/SimpleView/UIViewController+SimplePresent.m
@@ -13,7 +13,6 @@
 
 static char keyPresentWillDismissBlock;
 static char keyPresentDidDismissBlock;
-static char keyPresenterViewController;
 
 -(instancetype)resetModalTransitionStyle:(UIModalTransitionStyle)style{
     self.modalTransitionStyle = style;
@@ -23,19 +22,18 @@ static char keyPresenterViewController;
 -(void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion willDismissCallback:(PresentBlock)willDismissCallback didDismissCallback:(PresentBlock)didDismissCallback{
     objc_setAssociatedObject(self, &keyPresentWillDismissBlock, willDismissCallback, OBJC_ASSOCIATION_COPY_NONATOMIC);
     objc_setAssociatedObject(self, &keyPresentDidDismissBlock, didDismissCallback, OBJC_ASSOCIATION_COPY_NONATOMIC);
-    UIViewController *viewController = viewControllerToPresent;
-    if ([viewController isKindOfClass:[UINavigationController class]]) {
-        viewController = ((UINavigationController *) viewControllerToPresent).topViewController;
-    }
-    objc_setAssociatedObject(viewController, &keyPresenterViewController, self, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     [self presentViewController:viewControllerToPresent animated:flag completion:completion];
 }
 
 -(void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion success:(BOOL)success info:(id)info{
-    __weak __typeof(self) wself = self;
-    UIViewController *presenterViewController = objc_getAssociatedObject(wself, &keyPresenterViewController);
-    PresentBlock willDismissBlock = (PresentBlock)objc_getAssociatedObject(presenterViewController, &keyPresentWillDismissBlock);
-    PresentBlock didDismissBlock = (PresentBlock)objc_getAssociatedObject(presenterViewController, &keyPresentDidDismissBlock);
+    
+    UIViewController *vc = self.presentingViewController;
+    if ([vc isKindOfClass:[UINavigationController class]]) {
+        vc = ((UINavigationController *)self.presentingViewController).topViewController;
+    }
+    
+    PresentBlock willDismissBlock = (PresentBlock)objc_getAssociatedObject(vc, &keyPresentWillDismissBlock);
+    PresentBlock didDismissBlock = (PresentBlock)objc_getAssociatedObject(vc, &keyPresentDidDismissBlock);
     if (willDismissBlock) {
         willDismissBlock(success,info);
     }
