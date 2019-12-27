3a96ee94c54c311b11d371323843fd576eaaf48b
leo <work.mileo@gmail.com>
2019-12-27 14:21:16 +0800

    去掉log

--- SimpleView/LRVCStyle/UIViewController+LRFViewControllerStyle.m
+++ SimpleView/LRVCStyle/UIViewController+LRFViewControllerStyle.m
@@ -41,12 +41,11 @@ NSString *lrf_defaultStyleIdentifier;
 }
 
 - (void)LRFViewControllerStyle_viewDidLoad{
+    [self LRFViewControllerStyle_viewDidLoad];
     if (self.lrf_isVisible || self.lrf_viewControllerByPresent.lrf_isVisible) {
         void (^Block)(UIViewController *vc) = lrf_styles[self.lrf_styleIdentifier];
         Block(self);
     }
-    [self LRFViewControllerStyle_viewDidLoad];
-    NSLog(@"%d",[self lrf_isVisible]);
 }
 
 #pragma mark - set get
